5d45caa37ce3117bbbb441784c4bf0a4a5a1f6e0
Kent Le <le6790@gmail.com>
2023-04-27 00:14:14 -0600

    Finished more_conditionals and world_of_numbers

---
+++ world_of_number.sh
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+# https://unix.stackexchange.com/questions/306111/what-is-the-difference-between-the-bash-operators-vs-vs-vs
+
+read x
+read y
+
+# output sum of x and y
+echo $((x+y))
+# output difference of x and y 
+echo $((x-y))
+# output product of x and y
+echo $((x*y))
+# output quotient of x and y
+echo $((x/y))
